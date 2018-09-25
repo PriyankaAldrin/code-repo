f6290bcabbd920008bbdb343daa6ebfbfe9b48dc
Priyanka Aldrin <priyankaaldrin@gmail.com>
2018-09-25 07:15:02 +0530

    Add Max Multiples Code Completed Commit

--- AddMaxMutlipes.java
+++ AddMaxMutlipes.java
@@ -3,13 +3,14 @@ public class AddMaxMutlipes {
 
 	public static void main(String[] args) {
 		int[] a = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};  
-		int maxTotal = 0;
-		int s = 0;
-		for(int i=1; i<=(a.length-1) ; i++) {
-			int total = 0;
+		int startingIndex = 0;
+		int endingIndex = 14;
+		int maxTotal = a[startingIndex];
+		for(int i=startingIndex+1; i<=endingIndex ; i++) {
+			int total = a[startingIndex];
 			int j = i;
-			if(a[i]%a[s] == 0) {
-				for(int p=s; p<=(a.length-1) ; p++) {
+			if(a[i]%a[startingIndex] == 0) {
+				for(int p=startingIndex; p<=endingIndex ; p++) {
 				if(a[p]%a[j] ==0 ){
 				total += a[p];
 				j = p;
@@ -17,8 +18,7 @@ public class AddMaxMutlipes {
 		}
 			maxTotal = (maxTotal < total) ? total : maxTotal;
 		}
-		System.out.println(maxTotal);
 	}
-
+		System.out.println(maxTotal);
 }
 }
